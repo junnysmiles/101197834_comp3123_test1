6b2be84add77af0b0afe708995e194740df2401b
Jun Yan Gan <Jun-Yan.Gan@georgebrown.ca>
2021-10-05 12:44:40 -0400

    updated minor thing in question 2, completed question 1

--- question-1/question_one.js
+++ question-1/question_one.js
@@ -4,26 +4,39 @@
 
 // Question 1 - ES6 Features
 // Create a script with a function named lowerCaseWords that takes a mixed array as input.
-/* The function will do the following: return a promise that i resolved or reject,
+/* The function will do the following: return a promise that is resolved or rejected,
     filter the non-string and lower case the remaining words */
 
+// Creating lowerCaseWords function and taking array as input
 lowerCaseWords = (array) => {
-    const newArr = array.filter(e => typeof e == 'string')
-    const lowerCaseArr = newArr.map(e => e.toLowerCase())
+    // Initializing a new array to push the updated elements after doing the following...
+    let newArr = []
 
+    // Promise to resolve or reject, filter the non-strings and lower case the words
     let promise = new Promise(function(resolve, reject) {
-        if(lowerCaseArr)
-            resolve(`Success! Here is the new array: ${array}`)
-        else
-            reject("Failure")
-    })
+        // Going through each element in the array
+        for (let i = 0; i < array.length; i++) {
+            // If statement to find type of (string) in the element, then if it is a string, push the elements to the new array initialized
+            if (typeof array[i] === "string") {
+                // Push the string elements to the new array, then lowercase
+                newArr.push(array[i].toLowerCase())
+            }
+        }
+        // Resolve
+        if (array.length > 0) {
+            resolve(newArr)
+        }
+        // Reject
+        else {
+            reject("There are no strings in the array.")
+        }
+    });
+    return promise;
 }
 
 const mixedArray = ['PIZZA', 10, true, 25, false, 'Wings']
 
-const newArr = mixedArray.filter(e => typeof e == 'string')
-const lowerCaseArr = newArr.map(e => e.toLowerCase())
-
-console.log(lowerCaseArr)
-
-console.log(lowerCaseWords(mixedArray))
+// Handling the promise
+lowerCaseWords(mixedArray).then(result => {
+    console.log(result)
+}), error => console.log(error)
